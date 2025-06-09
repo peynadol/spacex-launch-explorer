a474455785e5af8cef0fd37c5865b028f24bdb55
peynadol <peynadol@gmail.com>
2025-06-09 16:15:35 +0100

    get youtube link embedded

--- src/pages/LaunchDetailPage.tsx
+++ src/pages/LaunchDetailPage.tsx
@@ -1,7 +1,31 @@
-import React from "react";
+import { useQuery } from "@tanstack/react-query";
+import { useParams } from "react-router";
+import { postQuery } from "../../spacexClient";
+import LiteYouTubeEmbed from "react-lite-youtube-embed";
 
 const LaunchDetailPage = () => {
-  return <div>I AM THE DETAILS PAGE</div>;
+  const { id } = useParams();
+  const { data: launch, isPending } = useQuery({
+    queryKey: ["launch", id],
+    queryFn: () =>
+      fetch(`https://api.spacexdata.com/v5/launches/${id}`).then((res) =>
+        res.json()
+      ),
+    enabled: !!id,
+  });
+  console.log(launch);
+
+  if (isPending) {
+    return <div>Loading...</div>;
+  }
+  return (
+    <div>
+      <LiteYouTubeEmbed
+        id={launch.links.youtube_id}
+        title={launch.name}
+      />
+    </div>
+  );
 };
 
 export default LaunchDetailPage;
